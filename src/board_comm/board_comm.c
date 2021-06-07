5629cbfd31ceae12ddac0d377362512d5af58558
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2021-06-07 10:21:55 -0600

    Update board_comm.c

--- src/board_comm/board_comm.c
+++ src/board_comm/board_comm.c
@@ -157,7 +157,7 @@ static void run_command(volatile imufCommand_t* command, volatile imufCommand_t*
                 filterConfig.ptnFilterType       = (int16_t) ((int16_t)(command->param10 >> 16));
                 if (!filterConfig.ptnFilterType || filterConfig.ptnFilterType > 4)
                 {
-                	filterConfig.ptnFilterType = 2;
+                	filterConfig.ptnFilterType = 3;
                 }
                 filterConfig.acc_lpf_hz          = (int16_t)(command->param10 & 0xFFFF);
                 if (!filterConfig.acc_lpf_hz)
